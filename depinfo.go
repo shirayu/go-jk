9a901e0fc2989674ed51dd23f2d92bed115a65c9
Yuta Hayashibe <yuta@hayashibe.jp>
2016-01-21 15:51:24 +0900

    Split a file

--- depinfo.go
+++ depinfo.go
@@ -37,20 +37,3 @@ func NewDependencyInfo(line string) (*DependencyInfo, error) {
 
 	return depi, err
 }
-
-//DependencyInfos is a slice of DependencyInfo
-type DependencyInfos []*DependencyInfo
-
-//GetPredRep returns the "rep" for the DependencyInfo
-func (depi *DependencyInfo) GetPredRep() string {
-	pname, ok := depi.Features["用言代表表記"]
-	if !ok {
-		return ""
-	}
-	vtype, ok := depi.Features["用言"]
-	if !ok {
-		return ""
-	}
-
-	return pname + ":" + vtype
-}
